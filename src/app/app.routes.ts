f3058b8339473c868c140ece7941e0bc508bf150
Ricardo Domingues <ricardo.domingues27@gmail.com>
2024-08-31 13:56:05 -0300

    change name module to pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PrateleiraComponent } from '../pages/prateleira/prateleira.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: PrateleiraComponent },
+];
