c00c037cf62f900e0a836129f8703a805fa543b7
RicardoDomingues27 <ricardo.domingues27@gmail.com>
2024-09-01 17:34:58 -0300

    config env and json server

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
-import { PrateleiraComponent } from '../pages/prateleira/prateleira.component';
+import { PrateleiraComponent } from './pages/prateleira/prateleira.component';
+
 
 export const routes: Routes = [
     { path: '', component: PrateleiraComponent },
